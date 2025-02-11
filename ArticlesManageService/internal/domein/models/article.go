b4b2b8e2645fb539d26910a2c8bab753e918ffe8
Artem Isachenkov <artemyt237@gmail.com>
2025-02-11 17:28:44 +0300

    Merge branch 'task-articlessManageService'

--- ArticlesManageService/internal/domein/models/article.go
+++ ArticlesManageService/internal/domein/models/article.go
@@ -9,8 +9,10 @@ import (
 type Article struct {
 	ID        uuid.UUID `json:"id" gorm:"type:uuid;primary_key;"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	OwnerID   uuid.UUID `json:"owner_id" gorm:"type:uuid;not null"`
 	Title     string    `json:"title" gorm:"size:255;not null"`
 	Content   string    `json:"content" gorm:"type:text;not null"`
+	Tags      []string  `json:"tags" gorm:"-"`
 	UpVote    int       `json:"up_vote" gorm:"default:0"`
 	DownVote  int       `json:"down_vote" gorm:"default:0"`
 	Comments  []Comment `json:"comments" gorm:"-"`
@@ -19,7 +21,7 @@ type Article struct {
 type Comment struct {
 	ID        uuid.UUID `json:"id" gorm:"type:uuid;primary_key;"`
 	OwnerID   uuid.UUID `json:"owner_id" gorm:"type:uuid;not null"`
-	PostID    uuid.UUID `json:"post_id" gorm:"type:uuid;not null"`
+	ArticleID uuid.UUID `json:"article_id" gorm:"type:uuid;not null"`
 	Content   string    `json:"content" gorm:"type:text;not null"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 }
